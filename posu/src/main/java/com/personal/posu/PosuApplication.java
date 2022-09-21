7b22bab2d37c058241759d6af78717a6bc93729f
Shaun Leonar <81700189+ScriBrannauts@users.noreply.github.com>
2022-09-21 19:34:01 +0800

    Add files via upload

---
+++ posu/src/main/java/com/personal/posu/PosuApplication.java
@@ -0,0 +1,13 @@
+package com.personal.posu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosuApplication.class, args);
+	}
+
+}
---
+++ posu/src/test/java/com/personal/posu/PosuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.posu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
